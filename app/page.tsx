110999e493c21e3861d540167d330dbe742d19f8
alex <siveraa@gmail.com>
2023-05-29 10:10:52 -0400

    cpb

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,133 @@
-import Image from 'next/image'
+"use client";
+import Image from 'next/image';
+import React, { useState, useEffect, useRef } from 'react';
+import "../components/SingleCard"
+import SingleCard from '../components/SingleCard';
+
+
+
+const images = [
+  { src: "/m1.png" , matched: false},
+  { src: "/m2.png" , matched: false },
+  { src: "/m3.png"  , matched: false},
+  { src: "/m4.png"  , matched: false},
+  { src: "/m5.png"  , matched: false},
+ 
+];
+
+
+interface Card {
+  id: number;
+  src: string;
+  matched: boolean
+}
 
 export default function Home() {
+  const [cards, setCards] = useState<Card[]>([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState<Card | null>(null);
+  const [choiceTwo, setChoiceTwo] = useState<Card | null>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [disabled, setDisabled] = useState(false);
+
+  const duplicateAndShuffleCards = () => {
+    const melangeC = [...images, ...images]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(melangeC);
+    setChoiceOne(null);
+    setChoiceTwo(null)
+    setTurns(0);
+  };
+
+  useEffect(() => {
+    duplicateAndShuffleCards()
+
+  },[])
+
+
+  useEffect(() => {
+    const updateContainerStyles = () => {
+      if (containerRef.current) {
+        const windowWidth = window.innerWidth;
+        const gridWidth = (1 / 2) * windowWidth - 40; // Subtracting 40px to account for the padding
+
+        const numColumns = 5; // Set the number of columns to 5
+        const columnWidth = `${Math.floor(gridWidth / numColumns)}px`;
+
+        containerRef.current.style.display = 'grid';
+        containerRef.current.style.gridTemplateColumns = `repeat(${numColumns}, ${columnWidth})`;
+        containerRef.current.style.padding = '20px'; // Set consistent padding
+        containerRef.current.style.gridGap = '10px'; // Set consistent gap between pictures
+        containerRef.current.style.overflow = 'hidden'; // Hide overflow to prevent scrolling
+      }
+    };
+
+    window.addEventListener('resize', updateContainerStyles);
+    updateContainerStyles();
+
+    return () => {
+      window.removeEventListener('resize', updateContainerStyles);
+    };
+  }, []);
+
+  useEffect(() => {
+  
+    if (choiceOne && choiceTwo) {
+      setDisabled(true)
+      if (choiceOne.src === choiceTwo.src) {
+        console.log("It's a match");
+        setCards(prevCards => {
+          return prevCards.map(card=> {
+            if (card.src === choiceOne.src) {
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn();
+      } else {
+        console.log("Don't match");
+        setTimeout( () =>  resetTurn(), 1000)
+       
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+    setDisabled(false)
+  };
+
+  const handleChoice = (card: Card) => {
+    if(card.id === choiceOne?.id) return;
+    if (choiceOne) {
+      setChoiceTwo(card);
+    } else {
+      setChoiceOne(card);
+    }
+  };
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="p-20">
+      <button onClick={duplicateAndShuffleCards} className="mb-8">New Game</button>
+      <div className="cards-container" ref={containerRef}>
+        {cards.map((card) => (
+          <SingleCard key={card.id}
+                       card={card}
+                       handleChoice={handleChoice}
+                       flipped={ card === choiceOne || card === choiceTwo || card.matched}
+                       disabled= {disabled }
+                       />
+        
+        ))}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <p> Nb essai : { turns } </p>
+    </div>
+  );
 }
---
+++ components/SingleCard.js
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import { useState } from 'react';
+import './card.css'
+
+export default function SingleCard({ card, handleChoice, flipped , disabled}) {
+
+
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+
+  return (
+
+
+    <div className='card'>
+        <div className={ flipped ? "flipped" : ""}>
+            <img className='front' src={card.src} alt="front" />
+            <img className='back'
+                 src="/back.png"
+                 onClick={handleClick}
+                 alt="back"
+                 />
+
+
+        </div>
+
+
+    </div>
+
+  );
+}
